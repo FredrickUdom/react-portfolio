8055f9f3d1b9fd6ceeb0f90ba74537f5447beebc
fredrickUdom <fredrickudom@gmail.com>
2024-03-25 16:16:59 +0100

    setting up font awesome Icons

--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -6,6 +6,21 @@ function Profile() {
             <div className="profile-parent">
                 <div className="profile-details">
                     <div className="colz">
+                        <a href="">
+                            <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i className="fa fa-google-plus-square" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i className="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i className="fa fa-youtube-square" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i className="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
                         
                     </div>
                 </div>
