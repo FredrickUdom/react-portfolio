27c1af6743173973d531042941164f1624465345
fredrickUdom <fredrickudom@gmail.com>
2024-03-28 15:46:43 +0100

    rendering footer in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './PortfolioContainer/Home/Profile'
+import Footer from './PortfolioContainer/Home/Footer/Footer'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
      </button> */}
 
      <Profile/>
+     <Footer/>
     
     </>
   )
