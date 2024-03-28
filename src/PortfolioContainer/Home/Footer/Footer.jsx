8c57b2a6478d03f5a3b7d2656d33abc3e74d4bcf
fredrickUdom <fredrickudom@gmail.com>
2024-03-28 15:29:46 +0100

    adding footer

---
+++ src/PortfolioContainer/Home/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+const Footer = () => {
+    return (  
+        <div className="footer-container">
+            <div className="footer-parent">
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Footer;
