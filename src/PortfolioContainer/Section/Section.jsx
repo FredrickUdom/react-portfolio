2b7912c32695937380a60e62a7cd5220aacb9728
fredrickUdom <fredrickudom@gmail.com>
2024-04-03 21:44:43 +0100

    fixing section area

--- src/PortfolioContainer/Section/Section.jsx
+++ src/PortfolioContainer/Section/Section.jsx
@@ -5,22 +5,25 @@ import pix2 from '../../assets/Home/pix2.jpg'
 import "./Section.css";
 
 function Section() {
-    return (
-        <div id="about" className=''>
-            <div className="about-parent pt-5">
-			<Container>
-                <h2 className='text-center'>About</h2>
-                <Row>
-                    <Col>
-                    <img src={pix2} alt="picture here" srcset="" />
-                    </Col>
-                    <Col>
-                    <div class="col-md-6 col-lg-7 pl-lg-5 pb-5">
+	return (
+		<section class="ftco-about img ftco-section ftco-no-pb" id="about-section">
+    	<div class="container">
+    		<div class="row d-flex">
+    			<div class="col-md-6 col-lg-5 d-flex">
+    				<div class="img-about img d-flex">
+    					<div class="overlay"></div>
+	    				<div class="img d-flex align-self-stretch align-items-center">
+							<img src={pix2} alt="" srcset="" />
+	    				</div>
+    				</div>
+    			</div>
+    			<div class="col-md-6 col-lg-7 pl-lg-5 pb-5">
     				<div class="row justify-content-start pb-3">
 		          <div class="col-md-12 heading-section ftco-animate ">
-		            <h2 class="mb-4 ">About Me</h2>
-		            <p>I write server-side logic with nestjs using SQL and NoSQL database. I am passionate to learn new things and to improve my skill as a Backend Developer.</p>
-		            <ul class="about-info mt-4 px-md-0 px-2 justify-content-between align-items-center">
+		            <h2 class="mb-4">About Me</h2>
+		           <div className="shadow px-3">
+				   <p>I write server-side logic with nestjs using SQL and NoSQL database. I am passionate to learn new things and to improve my skill as a Backend Developer.</p>
+		            <ul class="about-info mt-4 px-md-0 px-2">
 		            	<li class="d-flex"><span>Name:</span> <span>Fredrick Udom</span></li>
 		            	<li class="d-flex"><span>Date of birth:</span> <span>December 12, </span></li>
 		            	<li class="d-flex"><span>Address:</span> <span>Uyo, Nigeria</span></li>
@@ -28,6 +31,7 @@ function Section() {
 		            	<li class="d-flex"><span>Email:</span> <span>fredrickudom@gmail.com</span></li>
 		            	<li class="d-flex"><span>Phone: </span> <span>+234-705-767-9807</span></li>
 		            </ul>
+				   </div>
 		          </div>
 		        </div>
 	          <div class="counter-wrap ftco-animate d-flex mt-md-3">
@@ -40,13 +44,10 @@ function Section() {
               </div>
 	          </div>
 	        </div>
-                    </Col>
-                    
-                </Row>
-            </Container>
-			</div>
         </div>
-    );
+    	</div>
+    </section>
+	);
 }
 
 export default Section;
