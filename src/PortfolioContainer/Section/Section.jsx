bc85138e53b969140eeafad5deef864d12ff22d9
fredrickUdom <fredrickudom@gmail.com>
2024-04-03 16:37:02 +0100

    working on section component

--- src/PortfolioContainer/Section/Section.jsx
+++ src/PortfolioContainer/Section/Section.jsx
@@ -1,30 +1,24 @@
-const Section = () => {
-    return ( 
-        
-        <div id="skills">
-            <h1>this is my skill page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio totam neque quas ab nulla quo ducimus aut, accusantium, vitae atque fugit nemo consequuntur, quae blanditiis? Dolore quam aut repudiandae quo.
-            Impedit enim doloremque dolore! Praesentium sint perferendis qui libero id saepe eligendi? Minima accusantium possimus praesentium, a exercitationem error dolore quas quos vero id eveniet, voluptate est veniam sunt illum.
-            Magni aliquid enim impedit, vero eveniet, dolorum nulla, aperiam eos dolores animi numquam. Nisi ut illo, ipsam iste voluptatem inventore porro molestiae perferendis adipisci fugit quis! Aliquid quidem aperiam aut.
-            Atque nisi facere consequuntur sed hic soluta ipsa, perferendis eaque rerum provident, nulla molestiae laborum iusto quos, saepe animi cumque. Illum impedit, adipisci labore magni unde culpa minima corrupti laudantium!
-            Qui, cum possimus ullam quam vero officia accusamus est dolore quos dolores ut, exercitationem itaque mollitia placeat pariatur, necessitatibus blanditiis sapiente. Optio consectetur pariatur nam tempore dicta. Dolore, tenetur animi.
-            Nulla quia molestiae consequuntur magnam necessitatibus eligendi placeat ratione repellat quaerat repellendus, quod temporibus ipsa? Eius consectetur non nesciunt commodi exercitationem delectus in, sed soluta asperiores culpa quidem laudantium impedit!
-            Quam, consequuntur fugit amet eius fugiat iusto ipsam debitis nostrum sequi tenetur dolores unde itaque maiores quas autem quod at voluptatem, nisi beatae, delectus sapiente ea ipsa in! Vel, recusandae!
-            Saepe facere incidunt error aperiam, adipisci doloribus dignissimos commodi ea dolorem fuga pariatur atque natus temporibus similique facilis praesentium nostrum cupiditate molestiae. Architecto voluptate est deserunt fuga neque dicta esse.
-            Enim vel voluptates ratione necessitatibus dolore, nesciunt harum expedita explicabo tempora omnis corrupti, aperiam ipsum aliquid consequatur eaque quae porro quibusdam itaque commodi? Doloremque autem veniam, iure rerum eveniet illo!
-            Quo enim dolore reiciendis, magni quia veritatis vero ea saepe, est necessitatibus officiis ipsum nihil recusandae, voluptatum nisi ipsa debitis voluptatibus ab. Magni tempora numquam dolores dolorum nam suscipit incidunt.
-            Fugiat alias, quia temporibus accusantium minus, consequuntur dignissimos cupiditate voluptatibus aliquam voluptatum eaque suscipit quisquam id! Optio maxime porro illo accusantium consequuntur voluptatum neque autem, praesentium dolorum cupiditate natus atque!
-            Magni nihil molestiae possimus dolorem. Quam veritatis hic nemo tempora dolorem? Tenetur inventore cupiditate iusto, maxime possimus minima, corrupti molestias officiis iure molestiae explicabo quibusdam, architecto vero debitis! Quidem, laudantium!
-            Repellat aut consequuntur in, esse magni veritatis sint dolores praesentium officia nobis officiis et eligendi, quidem saepe unde animi odit. Architecto necessitatibus eaque officia voluptate magni fugiat facilis modi ratione.
-            Reprehenderit accusantium laborum minus quasi id officia provident voluptas, fuga repellendus praesentium fugiat voluptatem dolores magnam rerum mollitia architecto ipsam vitae sequi enim saepe. Quos nobis officia enim commodi dolore?
-            Iste neque facilis distinctio! Accusantium nam culpa quasi repellendus incidunt est mollitia nulla facilis expedita velit molestiae eum, hic delectus dolor rerum, amet animi. Voluptates assumenda saepe quas officia commodi!
-            Consectetur, quos autem repellat explicabo asperiores eligendi blanditiis illum doloremque sequi velit perspiciatis quod facere exercitationem hic amet est nam! Impedit, ratione! Iure explicabo quod, consequatur dignissimos assumenda at odit?
-            Velit dignissimos hic delectus obcaecati a dolores debitis ducimus veniam omnis voluptate minima consequatur maxime cum qui magni vitae quia, iusto animi temporibus labore excepturi accusantium. Ipsam, vitae minima. Nihil.
-            Delectus cum consectetur, adipisci dolor iste ipsum. Expedita, veritatis molestias earum hic, nemo temporibus fuga cupiditate, laborum dicta incidunt aperiam modi assumenda dignissimos labore! Nihil voluptate fugit suscipit quod est.
-            Commodi a cum adipisci temporibus veniam quibusdam. Ex illo harum eum eius itaque quas cum sit ullam, cumque iusto, omnis facere tempora id, nihil suscipit adipisci laborum nulla unde blanditiis!
-            Optio omnis aliquam doloribus eligendi ducimus eveniet consectetur laudantium non itaque quasi reprehenderit placeat vel sapiente dolore delectus distinctio quisquam ipsum incidunt architecto libero, exercitationem soluta quam eaque numquam. Cumque?</p>
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import pix2 from '../../assets/Home/pix2.jpg'
+// import "./Section.css";
+
+function Section() {
+    return (
+        <div id="about">
+            <Container>
+                <h2>About Me</h2>
+                <Row>
+                    <Col>
+                    <img src={pix2} alt="picture here" srcset="" />
+                    </Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
         </div>
-     );
+    );
 }
- 
+
 export default Section;
