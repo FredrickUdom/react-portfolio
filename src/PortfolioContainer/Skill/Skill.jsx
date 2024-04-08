72912c43e13a86d54e9b766a1ed0a81391f961c4
fredrickUdom <fredrickudom@gmail.com>
2024-04-08 15:11:22 +0100

    fixing services

--- src/PortfolioContainer/Skill/Skill.jsx
+++ src/PortfolioContainer/Skill/Skill.jsx
@@ -3,63 +3,59 @@ import nestjs from '../../assets/img/nestjs.png';
 import typescript from '../../assets/img/typescript_node.png';
 import './Skill.css'
 const Skill = () => {
-    return ( 
+	return (
 
-       <div className="skill">
-        
+		<div className="skill">
 
-        
-	<section class="ftco-section" id="services-section">
-		<div class="container">
-			<div class="row justify-content-center py-5 mt-5">
-				<div class="col-md-12 heading-section text-center ftco-animate">
-					<h2 class="big big-2"><p>Services</p></h2>
-					{/* <h2 class="mb-4">Services</h2> */}
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-				</div>
-			</div>
 
-			<div class="row justify-content-between ">
-				<div class="col-md-3 text-center d-flex ftco-animate shadow col-sm-8">
-					<a href="#" class="services-1">
-						<span class="">
-							 <i class="flaticon-analysis"></i> 
-							<img src={nestjs} alt="" srcset="" />
-						</span>
-						<div class="desc">
-							<h3 class="mb-5">I write Nestjs for building and developing scalable and maintainable
-								server-side APIs for both web applications and mobile applications</h3>
+
+			<section class="ftco-section" id="services-section">
+				<div class="container">
+					<div class="row justify-content-center py-5 mt-5">
+						<div class="col-md-12 heading-section text-center ftco-animate">
+							<h2 class="big big-2"><p>Services</p></h2>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
 						</div>
-					</a>
-				</div>
-				<div class="col-md-3 text-center d-flex ftco-animate shadow col-sm-8">
-					<a href="#" class="services-1">
-						<span class="icon">
-							<img src={typescript} alt="" srcset="" />
-						</span>
-						<div class="desc">
-							<h3 class="mb-5">I write Typescript and Nodejs when working on a project in Nestjs</h3>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-3 text-center d-flex ftco-animate shadow col-sm-8">
-					<a href="#" class="services-1">
-						<span class="icon">
-							<img src={docker} alt="" srcset="" />
-						</span>
-						<div class="desc">
-							<h3 class="mb-5">Using docker container for building, shipping, and running applications in
-								containers making it lightweightand portable.</h3>
-						</div>
-					</a>
-				</div>
+					</div>
 
-			</div>
+					<div class="row">
+						<div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
+							<a href="#" class="services-1 w-100">
+								<span class="icon">
+									<img src={nestjs} alt="" class="img-fluid" />
+								</span>
+								<div class="desc">
+									<h3>I write Nestjs for building scalable server-side APIs</h3>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
+							<a href="#" class="services-1 w-100">
+								<span class="icon">
+									<img src={typescript} alt="" class="img-fluid" />
+								</span>
+								<div class="desc">
+									<h3>I use Typescript and Nodejs with Nestjs</h3>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 text-center d-flex ftco-animate shadow">
+							<a href="#" class="services-1 w-100">
+								<span class="icon">
+									<img src={docker} alt="" class="img-fluid" />
+								</span>
+								<div class="desc">
+									<h3>Using Docker for containerization</h3>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
 		</div>
-	</section>
-
-       </div>
-     );
+	);
 }
- 
+
 export default Skill;
